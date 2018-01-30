4aa35d4cd43fdc0114b8a6d68b19243e792c5889
infining <nathanjgaul@gmail.com>
2018-01-29 20:52:28 -0700

    Changing colors, fonts

---
+++ app/src/main/java/com/nathanjgaul/android/a30_days_of_kotlin_for_android_day_01/Main2Activity.kt
@@ -0,0 +1,34 @@
+package com.nathanjgaul.android.a30_days_of_kotlin_for_android_day_01
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main2.*
+import java.util.*
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        showRandomNumber()
+    }
+
+    companion object {
+        const val TOTAL_COUNT = "total_count"
+    }
+
+    fun showRandomNumber() {
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
+
+        val random = Random()
+        var randomInt = 0
+        if (count > 0) {
+            randomInt = random.nextInt(count + 1)
+        }
+
+        textView_random.text = Integer.toString(randomInt)
+
+        textView_label.text = getString(R.string.random_heading, count)
+    }
+}
--- app/src/main/java/com/nathanjgaul/android/a30_days_of_kotlin_for_android_day_01/MainActivity.kt
+++ app/src/main/java/com/nathanjgaul/android/a30_days_of_kotlin_for_android_day_01/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.nathanjgaul.android.a30_days_of_kotlin_for_android_day_01
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.textView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun toastMe (view: View) {
+        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
+        myToast.show()
+    }
+
+    fun countMe (view: View) {
+        // Get the value of the text view
+        val countString = textView.text.toString()
+
+        // Convert value to number and increment it
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        // Display the new value in the text view
+        textView.text = count.toString()
+    }
+
+    fun randomMe (view: View) {
+        // Create and intent to start a second activity
+        val randomIntent = Intent(this, Main2Activity::class.java)
+
+        val countString = textView.text.toString()
+
+        val count = Integer.parseInt(countString)
+
+        // Add count to the extras for the view Intent
+        randomIntent.putExtra(Main2Activity.TOTAL_COUNT, count)
+
+        // Start the new activity
+        startActivity(randomIntent)
     }
 }
